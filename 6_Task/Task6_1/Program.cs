19db099027da6a7639bde53a69ef43f3144b5465
StupidAI <Minenkoea@gmail.com>
2021-11-17 09:31:51 +0500

    task6 initial commin

---
+++ 6_Task/Task6_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_1
+{
+    /* Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. 
+     * Знаки препинания не используются. Найти самое длинное слово в строке.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // user input
+            Console.WriteLine("Enter text:\n");
+            string text = Console.ReadLine();
+            // split phrase to words
+            string[] words = text.Split();
+            // find longest
+            string longest = "";
+            foreach (string s in words)
+            {
+                longest = (longest.Length < s.Length) ? s : longest;
+            }
+            // output
+            Console.WriteLine($"\nLongest word is: {longest}");
+            Console.WriteLine("\nPress any button...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6_Task/Task6_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_3
+{
+    /* Дана строка S. Из строки требуется удалить текст, заключенный в фигурные скобки. 
+     * В строке может быть несколько фрагментов, заключённых в фигурные скобки. 
+     * Возможно использование вложенных фигурных скобок, необходимо, чтобы программа это учитывала.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // user input
+            //Console.WriteLine("Enter text:\n");
+            //string text = Console.ReadLine();
+            string text = "Текст1 {Дата}{{Дата}{Дата}} или {Дата}";
+            string result = "";
+            // delete spaces from string and convert to lower case
+            string[] splited = text.Split('{');
+            foreach (string s in splited)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine(splited);
+            Console.WriteLine("\nPress any button...");
+            Console.ReadKey();
+        }
+    }
+}
