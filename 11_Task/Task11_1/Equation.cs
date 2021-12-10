46f2a0e15320502608adf6208d25cbb7c970fd4d
StupidAI <minenkoea@gmail.com>
2021-12-10 13:35:08 +0500

    Task11_1 review

---
+++ 11_Task/Task11_1/Equation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_1
+{
+    struct Equation
+    {
+        // Autofields
+        public double K { get; set; }
+        public double B { get; set; }
+
+        // Constructor
+        public Equation(double k, double b)
+        {
+            this.K = k;
+            this.B = b;
+        }
+
+        //Method for solving equation
+        public void Root()
+        {
+            if (K == 0 && B == 0)
+            {
+                Console.WriteLine("X : can be any number");
+            }
+            if (K == 0 && B != 0)
+            {
+                Console.WriteLine("There is no solution");
+            }
+            if (K!=0)
+            {
+                Console.WriteLine($"0 = {K} * x + {B} -> x = {-B/K}");
+            }
+        }
+    }
+}
---
+++ 11_Task/Task11_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_1
+{
+    /*
+     * Разработать структуру для решения линейного уравнения 0=kx+b. 
+     * Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+     * Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры. 
+     * Осуществить использование экземпляра в программе.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // option 1 -> any X
+            Equation eq1 = new Equation(0, 0);
+            eq1.Root();
+
+            // option 2 -> No solution
+            Equation eq2 = new Equation(0, 1);
+            eq2.Root();
+
+            // option 3 -> x = -B/K
+            Equation eq3 = new Equation(1, 2);
+            eq3.Root();
+
+            // Close app
+            Console.WriteLine("\nPress any button");
+            Console.ReadKey();
+        }
+    }
+}
