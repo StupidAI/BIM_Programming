aa87f93d150e2971bdb55a94369885aef47cdcd6
StupidAI <minenkoea@gmail.com>
2021-12-10 19:42:14 +0500

    Task 15 review

---
+++ 15_Task/Task15_1/Interfaces.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15_1
+{
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+    class ArithProgression : ISeries
+    {
+        public ArithProgression (int startValue, int delta)
+        {
+            setStart(startValue);
+            Delta = delta;
+            CurrentValue = startValue;
+        }
+        public int StartValue { get; set; }
+        public int CurrentValue { get; set; }
+        public int Delta { get; set; }
+        public int getNext()
+        {
+            return CurrentValue += Delta;
+        }
+
+        public void reset()
+        {
+            CurrentValue = StartValue;
+        }
+
+        public void setStart(int x)
+        {
+            StartValue = x;
+        }
+    }
+    class GeomProgression : ISeries
+    {
+        public GeomProgression(int startValue, int delta)
+        {
+            setStart(startValue);
+            Delta = delta;
+            CurrentValue = startValue;
+        }
+        public int StartValue { get; set; }
+        public int CurrentValue { get; set; }
+        public int Delta { get; set; }
+        public int getNext()
+        {
+            return CurrentValue *= Delta;
+        }
+
+        public void reset()
+        {
+            CurrentValue = StartValue;
+        }
+
+        public void setStart(int x)
+        {
+            StartValue = x;
+        }
+    }
+}
---
+++ 15_Task/Task15_1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method for testing progression
+            void RunProgression(ISeries a, int num)
+            {
+                Console.WriteLine("Run progression...");
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine($"Step {i + 1}: " + a.getNext());
+                }
+            }
+
+            #region ArithProgression
+            // User input - ArithProgression
+            Console.WriteLine("=== ArithProgression ===");
+            Console.Write($"Enter start value: ");
+            int sv = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter delta: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter number of iterations: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // Create progression and run it N times
+            ArithProgression ap1 = new ArithProgression(sv, d);
+            RunProgression(ap1, n);
+
+            // set new start value and run N times
+            Console.Write($"\nEnter start value one more time: ");
+            sv = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Delta: {d}");
+            ap1.setStart(sv);
+            ap1.reset();
+            Console.WriteLine();
+            RunProgression(ap1, n);
+            #endregion
+
+            #region GeomProgression
+            // User input GeomProgression
+            Console.WriteLine("\n=== GeomProgression ===");
+            Console.Write($"Enter start value: ");
+            sv = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter delta: ");
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter number of iterations: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // Create progression and run it N times
+            GeomProgression gp1 = new GeomProgression(sv, d);
+            RunProgression(gp1, n);
+            Console.WriteLine();
+
+            // set new start value and run N times
+            Console.Write($"Enter start value one more time: ");
+            sv = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Delta: {d}");
+            gp1.setStart(sv);
+            gp1.reset();
+            Console.WriteLine();
+            RunProgression(gp1, n);
+            #endregion
+
+            // close app
+            Console.WriteLine("\nPress any key...");
+            Console.ReadKey();
+        }
+    }
+}
