20fb95f8fea40ad8f976cfb9c0b185e21561b717
StupidAI <Minenkoea@gmail.com>
2021-12-14 00:39:09 +0500

    Task 17 review

---
+++ 17_Task/Task17_1/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17_1
+{
+    /* 1.    Создать класс для моделирования счета в банке. Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.  
+     * Класс должен быть объявлен как обобщенный. Универсальный параметр T должен определять тип номера счета. 
+     * Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения. 
+     * Создать  несколько экземпляров класса, параметризированного различными типам. 
+     * Заполнить его поля и вывести информацию об экземпляре класса на печать.
+     */
+    class BankAccount<T>
+    {
+        //closed from outside fields and properties
+        T BankNum { get; set; }
+        double Bill { get; set; }
+        string UserName { get; set; }
+
+        // constructor
+        public BankAccount(string userName, T bankAccount, double bill)
+        {
+            UserName = userName;
+            BankNum = bankAccount;
+            Bill = bill;
+        }
+
+        // methods
+        public void SetName(string userName) // ability to change user name
+        {
+            UserName = userName;
+        }
+        public void SetBill(double bill) // ability to change bill
+        {
+            Bill = bill;
+        }
+        public void AddMoney(double money) // ability to give some money to account bill
+        {
+            Bill += money;
+        }
+
+        // get info about account
+        public void GetInfo()
+        {
+            Console.WriteLine("Bank account info:");
+            Console.WriteLine($"User name: {UserName}");
+            Console.WriteLine($"Bank account: {BankNum}");
+            Console.WriteLine($"Bill: {Bill}$\n");
+        }
+
+    }
+}
---
+++ 17_Task/Task17_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create first account
+            BankAccount<int> ba1 = new BankAccount<int>("User 1", 150150, 50);
+            ba1.GetInfo();
+            ba1.SetName("Ivan"); // change user name
+            ba1.SetBill(150000); // change money info
+            ba1.GetInfo();
+
+            // create second account
+            Console.WriteLine("Next Bank account:");
+            BankAccount<string> ba2 = new BankAccount<string>("Alex", "150-5151", 0);
+            ba2.GetInfo();
+            ba2.AddMoney(155.5); // add some money
+            ba2.GetInfo();
+
+            // close app
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
